a1298ab04ef164e3f3d9ecf83e2bef03f225d89e
TheDogsNose <>
2024-12-18 03:32:49 +0200

    clean up

--- source/sicx.cpp
+++ source/sicx.cpp
@@ -19,8 +19,6 @@ main(int argc, char *argv[])
   // 5. Handle incoming connections
   // 6. Clean up and exit
 
-  // std::list<std::variant<sicx::Instruction<sicx::Format1>, sicx::Instruction<sicx::Format2>, sicx::Instruction<sicx::Format34>, sicx::Instruction<sicx::Format4f>, sicx::Instruction<sicx::Data>, sicx::Instruction<sicx::Directive>>> lines;
-
   list<string> lines;
   list<sicx::Instruction> insts;
 
@@ -70,42 +68,6 @@ main(int argc, char *argv[])
     lines2.push_back(str);
   }
   WriteLines("", "pass1.txt", lines2);
-
-  // readFromFile("in.txt", lines);
-  /*auto it = lines.begin();
-
-   for (int i = 0; i < lines.size(); i++)
-   {
-     if (holds_alternative<sicx::Instruction<sicx::Format1>>(*it)){
-
-       std::cout << "Format1: " << (get<sicx::Instruction<sicx::Format1>>(*it)).label << std::endl;
-
-     }else if (holds_alternative<sicx::Instruction<sicx::Format2>>(*it)){
-       std::cout << "Format2: " << (get<sicx::Instruction<sicx::Format2>>(*it)).label << std::endl;
-
-     }else if (holds_alternative<sicx::Instruction<sicx::Format34>>(*it)){
-
-       std::cout << "Format3/4: " << (get<sicx::Instruction<sicx::Format34>>(*it)).label << std::endl;
-
-     }else if (holds_alternative<sicx::Instruction<sicx::Format4f>>(*it)){
-
-       std::cout << "Format4f: " << (get<sicx::Instruction<sicx::Format4f>>(*it)).label << std::endl;
-
-     }else if (holds_alternative<sicx::Instruction<sicx::Data>>(*it)){
-
-       std::cout << "FormatD: " <<( get<sicx::Instruction<sicx::Data>>(*it)).label << std::endl;
-
-     }else if (holds_alternative<sicx::Instruction<sicx::Directive>>(*it)){
-
-       std::cout << "Format#: " << (get<sicx::Instruction<sicx::Directive>>(*it)).label << std::endl;
-
-     }else{
-
-       std::cout << "Error" << std::endl;
-   }
-
-     std::advance(it, 1);
-   }*/
 
   cin.get();
 }
